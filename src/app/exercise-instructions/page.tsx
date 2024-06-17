410e34640834b0b2c5f87d9d8a18741223a847fc
Luka Martin <ldm3334@gmail.com>
2024-06-17 22:15:12 +1000

    Make changes to styling for mobile devices

--- src/app/exercise-instructions/page.tsx
+++ src/app/exercise-instructions/page.tsx
@@ -4,8 +4,8 @@ import ExerciseInstructionsContainer from "../components/ExerciseInstructionsCon
 export default function ExerciseExamples() {
   return (
     <section className="flex flex-col items-center">
-      <h2 className="text-lg md:text-3xl lg:text-4xl xl:text-5xl font-semibold pb-6">Exercise Instructions</h2>
-      <p className="pb-6 px-4 text-center text-sm md:text-base">Use the input below to get detailed instructions on the exercises in your training program</p>
+      <h2 className="text-xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold pb-6">Exercise Instructions</h2>
+      <p className="pb-6 px-4 text-center text-base">Use the input below to get detailed instructions on the exercises in your training program</p>
 
       <ExerciseInstructionsContainer />
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TrainingProgramContainer from "./components/TrainingProgramContainer";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-lg md:text-3xl lg:text-4xl xl:text-5xl font-semibold pb-6">
+      <h1 className="text-xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold pb-6">
         CREATE YOUR TRAINING PROGRAM
       </h1>
 
