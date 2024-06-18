6c11ebe3d24ae7bd4834f8e45dae895ffbd648b9
Luka Martin <ldm3334@gmail.com>
2024-06-18 15:29:16 +1000

    Edit element tags on exercise instructions page

--- src/app/exercise-instructions/page.tsx
+++ src/app/exercise-instructions/page.tsx
@@ -3,11 +3,11 @@ import ExerciseInstructionsContainer from "../components/ExerciseInstructionsCon
 
 export default function ExerciseExamples() {
   return (
-    <section className="flex flex-col items-center">
+    <main className="flex flex-col items-center">
       <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold pb-6">Exercise Instructions</h2>
       <p className="pb-6 px-4 text-center text-base">Use the input below to get detailed instructions on the exercises in your training program</p>
 
       <ExerciseInstructionsContainer />
-    </section>
+    </main>
   )
 }
