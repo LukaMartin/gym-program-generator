9cda0ce7204c402aad2fa39bcd3ee3415594100b
Luka Martin <ldm3334@gmail.com>
2024-06-21 21:44:53 +1000

    Edit exercise examples page paragraph text.

--- src/app/exercise-instructions/page.tsx
+++ src/app/exercise-instructions/page.tsx
@@ -6,7 +6,7 @@ export default function ExerciseExamples() {
     <main className="flex flex-col items-center">
       <H1 className="text-2xl">Exercise Instructions</H1>
       <p className="mb-20 px-4 text-center text-base text-white/75 italic">
-        Use the search below to get detailed instructions on the exercises in
+        Search below to get detailed instructions on the exercises in
         your training program
       </p>
 
